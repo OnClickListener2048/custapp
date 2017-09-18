ab33395b1fa3680c4db8ca75c8244db13f78ee49
liufei <liufei@icounting.cn>
2017-09-18 11:02:35 +0800

    我的页面UI完成

--- app/mine/mine/MinePage.js
+++ app/mine/mine/MinePage.js
@@ -25,8 +25,9 @@ export default class MinePage extends Component {
 
     render(){
         return(
-            <View style={{flex:1,backgroundColor:'#FFFFFF'}}>
+            <View style={{flex:1,backgroundColor:'#F9F9F9'}}>
                 <View style={styles.login_wrapper}>
+                    <View style={{flexDirection:'row'}}>
                     <Image style={styles.head_img} source={require('../../img/head_img.png')}/>
                     <View style={styles.login_title_wrapper}>
                         <Text style={styles.login}>
@@ -36,10 +37,42 @@ export default class MinePage extends Component {
                             请立即注册或登录
                         </Text>
                     </View>
+                    </View>
                     <Image style={styles.left_bu} source={require('../../img/left_button.png')}/>
-
-
                 </View>
+                <View style={styles.items}>
+                    <Text style={styles.items_text}>
+                        我的订单
+                    </Text>
+                    <Image style={styles.items_button} source={require('../../img/left_button.png')}/>
+                </View>
+                <View style={styles.line}/>
+                <View style={[styles.items,{marginTop:0}]}>
+                    <Text style={styles.items_text}>
+                        企业信息
+                    </Text>
+                    <Image style={styles.items_button} source={require('../../img/left_button.png')}/>
+                </View>
+                <View style={styles.items}>
+                    <Text style={styles.items_text}>
+                        账号与安全
+                    </Text>
+                    <Image style={styles.items_button} source={require('../../img/left_button.png')}/>
+                </View>
+                <View style={styles.line}/>
+                <View style={[styles.items,{marginTop:0}]}>
+                    <Text style={styles.items_text}>
+                        设置
+                    </Text>
+                    <Image style={styles.items_button} source={require('../../img/left_button.png')}/>
+                </View>
+                <View style={styles.items}>
+                    <Text style={styles.items_text}>
+                        联系客服
+                    </Text>
+                    <Image style={styles.items_button} source={require('../../img/left_button.png')}/>
+                </View>
+
 
             </View>
         )
@@ -53,14 +86,18 @@ const styles = StyleSheet.create({
     login_wrapper:{
         flexDirection:'row',
         width:SCREEN_WIDTH,
-        marginHorizontal:20,
-        marginTop:80,
+        paddingHorizontal:20,
+        paddingTop:80,
+        paddingBottom:20,
+        backgroundColor:'#FFFFFF',
+        justifyContent:'space-between'
     },
+
     head_img:{
         resizeMode: "contain",
     },
     login_title_wrapper:{
-        marginTop:15,
+        marginTop:13,
         marginLeft:10
     },
     login:{
@@ -74,17 +111,32 @@ const styles = StyleSheet.create({
     left_bu:{
         resizeMode: "contain",
         marginTop:20,
-        marginLeft:145
+        marginLeft:10
+    },
+    items:{
+        flexDirection:'row',
+        width:SCREEN_WIDTH,
+        paddingHorizontal:20,
+        backgroundColor:'#FFFFFF',
+        marginTop:10,
+        paddingVertical:20,
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    items_text:{
+        fontSize:16,
+        color:'#333333',
+
+    },
+    items_button:{
+        resizeMode: "contain",
     },
     line:{
         height:1,
         width:SCREEN_WIDTH,
         borderBottomColor:'#ececec',
         borderBottomWidth:0.5 ,
-        backgroundColor:'transparent',
-        marginLeft:10,
-        marginRight:10,
-        marginTop:5
+        backgroundColor:'transparent'
     },
 
 })
