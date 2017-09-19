c6db5a667d3272339c2c291e0b7c0f9f4b0a1f12
BeanSoft <beansoft@126.com>
2017-09-19 14:56:06 +0800

    fix jest test

--- setupJest.js
+++ setupJest.js
@@ -15,3 +15,11 @@ jest.mock('NetInfo', () => {
 
 jest.mock('react-native-device-info');
 jest.mock('react-native-alert');
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+import FormData from './__tests__/mocks/FormData';
+
+// 修复 FormData 找不到的问题 ReferenceError: FormData is not defined
+global.FormData = FormData;
