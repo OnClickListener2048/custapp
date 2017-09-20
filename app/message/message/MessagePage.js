ddbaf5479eeb2ac05bf8367823909e77223880fd
zhuangzihao <zhuangzh@i-counting.cn>
2017-09-20 11:55:46 +0800

    no message

--- app/message/message/MessagePage.js
+++ app/message/message/MessagePage.js
@@ -4,15 +4,71 @@
 import React, {Component} from 'react';
 import {
     View,
-    Text
 } from 'react-native';
-
+import CommonCell from '../../view/CommenCell'
+import UltimateListView from "react-native-ultimate-listview";
 export default class MessagePage extends Component {
-    render(){
-        return(
-            <View>
-                <Text>MessagePage</Text>
-            </View>
-        )
+    constructor(props) {
+        super(props);
     }
+
+    sleep = (time) => new Promise(resolve => setTimeout(() => resolve(), time));
+
+    onFetch = async (page = 1, startFetch, abortFetch) => {
+        try {
+            //This is required to determinate whether the first loading list is all loaded.
+            let pageLimit = 10;
+            let skip = (page - 1) * pageLimit;
+
+            //Generate dummy data
+            let rowData = Array.from({length: pageLimit}, (value, index) => `item -> ${index + skip}`);
+
+            //Simulate the end of the list if there is no more data returned from the server
+            if (page === 10) {
+                rowData = [];
+            }
+
+            //Simulate the network loading in ES7 syntax (async/await)
+            await this.sleep(2000);
+            startFetch(rowData, pageLimit);
+        } catch (err) {
+            abortFetch(); //manually stop the refresh or pagination if it encounters network error
+            console.log(err);
+        }
+    };
+
+
+
+
+
+    renderItem = (item, index, separator) => {
+        return(
+            <CommonCell
+                leftText="111"
+            />
+        )
+    };
+
+    renderPaginationFetchingView = () => {
+        return (
+            <View></View>
+        );
+    };
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <UltimateListView
+                    ref={(ref) => this.listView = ref}
+                    onFetch={this.onFetch}
+                    keyExtractor={(item, index) => `${index} - ${item}`}  //this is required when you are using FlatList
+                    refreshableMode="advanced" //basic or advanced
+                    item={this.renderItem}  //this takes three params (item, index, separator)
+                    paginationFetchingView={this.renderPaginationFetchingView}
+                />
+            </View>
+        );
+    }
+
+
+
 }
