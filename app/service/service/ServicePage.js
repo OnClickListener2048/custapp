0bbfffa0ca9ade42a7368dc56d2a3f05045d1082
liufei <liufei@icounting.cn>
2017-09-25 16:14:39 +0800

    服务页面UI

--- app/service/service/ServicePage.js
+++ app/service/service/ServicePage.js
@@ -6,7 +6,9 @@ import {
     View,
     Text,
     StyleSheet,
-    Image
+    Image,
+    TouchableOpacity,
+    ScrollView
 
 } from 'react-native';
 
@@ -25,8 +27,16 @@ export default class ServicePage extends BComponent {
     constructor(props) {
         super(props);
         this.state = {
+            isChoose1:true,
+            isChoose2:false,
+            isChoose3:false,
+            isChoose4:false,
+            isChoose5:false,
+            num:1
 
         };
+        this._renderBody=this._renderBody.bind(this);
+        this.setChoose=this.setChoose.bind(this);
     }
     static navigatorStyle = {
         navBarHidden: false, // 隐藏默认的顶部导航栏
@@ -35,7 +45,7 @@ export default class ServicePage extends BComponent {
 
     render(){
         return(
-            <View style={{flex:1,backgroundColor:'white'}}>
+            <ScrollView style={{flex:1,backgroundColor:'white'}}>
                 <Image style={{resizeMode : "contain"}} source={require('../../img/service_demo_bg.png')}/>
                 <View style={styles.wrapper1}>
                     <View style={[styles.line,{width:30}]}/>
@@ -45,29 +55,90 @@ export default class ServicePage extends BComponent {
                     <View style={[styles.line,{width:30}]}/>
                 </View>
 
-
-
-                {this.renderBody()}
-            </View>
+                <View style={styles.wrapper2}>
+                    <TouchableOpacity onPress={()=>{this.setChoose(1)}} >
+                    <Image style={styles.service_gray_bg} source={this.state.isChoose1 ?require('../../img/service_red_bg.png') : require('../../img/service_gray_bg.png')}>
+                        <Text style={this.state.isChoose1 ?styles.te_white : styles.te_black}>
+                            01
+                        </Text>
+                    </Image>
+                    </TouchableOpacity>
+                    <View style={[styles.line,{width:54,borderBottomColor:this.state.isChoose1?'#e13238':'#f0f0f0'}]}/>
+                    <TouchableOpacity onPress={()=>{this.setChoose(2)}} >
+                        <Image style={styles.service_gray_bg} source={this.state.isChoose2 ?require('../../img/service_red_bg.png') : require('../../img/service_gray_bg.png')}>
+                            <Text style={this.state.isChoose2 ?styles.te_white : styles.te_black}>
+                                02
+                            </Text>
+                        </Image>
+                    </TouchableOpacity>
+                    <View style={[styles.line,{width:54,borderBottomColor:this.state.isChoose2?'#e13238':'#f0f0f0'}]}/>
+                    <TouchableOpacity onPress={()=>{this.setChoose(3)}} >
+                        <Image style={styles.service_gray_bg} source={this.state.isChoose3 ?require('../../img/service_red_bg.png') : require('../../img/service_gray_bg.png')}>
+                            <Text style={this.state.isChoose3 ?styles.te_white : styles.te_black}>
+                                03
+                            </Text>
+                        </Image>
+                    </TouchableOpacity>
+                    <View style={[styles.line,{width:54,borderBottomColor:this.state.isChoose3?'#e13238':'#f0f0f0'}]}/>
+                    <TouchableOpacity onPress={()=>{this.setChoose(4)}} >
+                        <Image style={styles.service_gray_bg} source={this.state.isChoose4 ?require('../../img/service_red_bg.png') : require('../../img/service_gray_bg.png')}>
+                            <Text style={this.state.isChoose4 ?styles.te_white : styles.te_black}>
+                                04
+                            </Text>
+                        </Image>
+                    </TouchableOpacity>
+                    <View style={[styles.line,{width:54,borderBottomColor:this.state.isChoose4?'#e13238':'#f0f0f0'}]}/>
+                    <TouchableOpacity onPress={()=>{this.setChoose(5)}} >
+                        <Image style={styles.service_gray_bg} source={this.state.isChoose5 ?require('../../img/service_red_bg.png') : require('../../img/service_gray_bg.png')}>
+                            <Text style={this.state.isChoose5 ?styles.te_white : styles.te_black}>
+                                05
+                            </Text>
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+                {this._renderBody(this.state.num)}
+            </ScrollView>
         )
     }
-    renderBody(index){
+
+    setChoose(index){
         switch (index){
-            case 0:
-                return <CopyTaxes />
-                break
             case 1:
-                return <SendBill />
-                break
+                this.setState({isChoose1:true,num:1,isChoose2:false,isChoose3:false,isChoose4:false,isChoose5:false});
+                break;
             case 2:
-                return <AccountingTreatment />
-                break
+                this.setState({isChoose1:false,num:2,isChoose2:true,isChoose3:false,isChoose4:false,isChoose5:false});
+                break;
             case 3:
-                return <PayTaxes />
-                break
+                this.setState({isChoose1:false,num:3,isChoose2:false,isChoose3:true,isChoose4:false,isChoose5:false});
+                break;
             case 4:
+                this.setState({isChoose1:false,num:4,isChoose2:false,isChoose3:false,isChoose4:true,isChoose5:false});
+                break;
+            case 5:
+                this.setState({isChoose1:false,num:5,isChoose2:false,isChoose3:false,isChoose4:false,isChoose5:true});
+                break;
+        }
+    }
+
+
+    _renderBody(index){
+        switch (index){
+            case 1:
+                return <CopyTaxes />
+                break;
+            case 2:
+                return <SendBill />
+                break;
+            case 3:
+                return <AccountingTreatment />
+                break;
+            case 4:
+                return <PayTaxes />
+                break;
+            case 5:
                 return <ClearCard />
-                break
+                break;
         }
     }
 }
@@ -81,6 +152,14 @@ const styles = StyleSheet.create({
         justifyContent:'center'
 
     },
+    wrapper2:{
+        flexDirection:'row',
+        width:SCREEN_WIDTH,
+        justifyContent:'center',
+        marginTop:30,
+        alignItems:'center'
+
+    },
     line:{
         height:1,
         width:SCREEN_WIDTH,
@@ -88,5 +167,24 @@ const styles = StyleSheet.create({
         borderBottomWidth:1 ,
         backgroundColor:'transparent'
     },
+    service_gray_bg:{
+        resizeMode : "contain",
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    te_black:{
+        fontSize:12,
+        color:'#666666',
+        alignItems:'center',
+        backgroundColor:'transparent'
+    },
+    te_white:{
+        fontSize:12,
+        color:'#FFFFFF',
+        alignItems:'center',
+        backgroundColor:'transparent'
+
+    }
+
 
 });
