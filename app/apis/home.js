b9ece43d9af0f3b050545e895e5ba93bf29abd73
zhuangzihao <zhuangzh@i-counting.cn>
2017-09-29 11:24:04 +0800

    网络调试

--- app/apis/home.js
+++ app/apis/home.js
@@ -1,8 +1,13 @@
 /**
  * Created by zhuangzihao on 2017/9/29.
  */
-import {getApi} from './common';
+import {getApi,postApi} from './common';
 
 export function loadHomeData(type = '0') {
-    return getApi('/api/mock/columns',{type});
+    return getApi('api/mock/columns',{type});
+}
+
+
+export function postTest(type = '1',productname='111',city='北京',name='zzh',tel='111',text='222') {
+    return postApi('api/mock/feedbacks',{type,productname,city,name,tel,text});
 }
