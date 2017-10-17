fb72d9d33b0032642f1d6431eec285744e61f9ba
BeanSoft <beansoft@126.com>
2017-10-17 11:53:28 +0800

    update debug apk

--- build_android.sh
+++ build_android.sh
@@ -5,4 +5,5 @@ echo make android bundle
 rm -f android/app/build/outputs/apk/*.apk
 echo Building APK
 rm -rf node_modules/jpush-react-native/android/build
-cd android && ./gradlew assembleRelease assembleDebug
+cd android && ./gradlew assembleRelease
+./gradlew assembleDebug
