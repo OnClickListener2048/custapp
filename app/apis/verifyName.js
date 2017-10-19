00ea5a8e3b711d1df3141297834030b39aad195a
GraceJia <jiaxt@i-counting.cn>
2017-10-19 10:28:35 +0800

    api/mock更改为api/v1    企业概况

--- app/apis/verifyName.js
+++ app/apis/verifyName.js
@@ -4,19 +4,19 @@
 import {getApi} from './common';
 
 export function loadVerifyCode(mobile = '',type = '5') {
-    return getApi('/api/mock/verificode/send',{mobile,type});
+    return getApi('/api/vo/verificode/send',{mobile,type});
 }
 
 export function loadVerifyResultData(keyword = '',mobile = '',vcode = '') {
-    return getApi('/api/mock/companies/check',{keyword,mobile,vcode});
+    return getApi('/api/v1/companies/check',{keyword,mobile,vcode});
 }
 
 export function loadVerifyCompaniesList(keyword = '',page = 1,count = 10) {
-    return getApi('/api/mock/companies/search',{keyword,page,count});
+    return getApi('/api/v1/companies/search',{keyword,page,count});
 }
 
 export function loadVerifyCompanyInfo(mobile='') {
-    return getApi('/api/mock/companies',{mobile});
+    return getApi('/api/v1/companies',{mobile});
 }
 
 
--- app/mine/companyInfo/CompanyInfoPage.js
+++ app/mine/companyInfo/CompanyInfoPage.js
@@ -13,6 +13,30 @@ import BComponent from '../../base/BComponent'
 
 export default class CompanyInfoPage extends BComponent {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            company:null,
+        };
+    }
+
+    componentDidMount(){
+        UserInfoStore.getCompany().then(
+            (company) => {
+                console.log('company', company);
+                if (company && company.name) {
+                    this.setState({company: company.name});
+                } else {
+                    this.setState({company: ''});
+                }
+            },
+            (e) => {
+                console.log("读取信息错误:", e);
+            },
+        );
+
+    }
+
     render(){
         return(
             <View style={{flex:1,backgroundColor:'#F9F9F9'}}>
@@ -21,7 +45,7 @@ export default class CompanyInfoPage extends BComponent {
                 >
                     <CommenCell
                         leftText="企业概况"
-                        rightText="大佳科技"
+                        rightText={this.state.company}
                         style={{marginTop:10}}
                         onPress={this._goto.bind(this,'CompanySurveyPage','企业概况')}
                     />
