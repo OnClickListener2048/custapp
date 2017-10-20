6e07b7facd155d6d2bb509079202c475cf93aed9
zhuangzihao <zhuangzh@i-counting.cn>
2017-10-20 14:28:41 +0800

    服务更换接口 未调通

--- app/apis/service.js
+++ app/apis/service.js
@@ -7,27 +7,27 @@ export function loadServiceData(companyid,date='') {
 
     let params = companyid?{companyid,date}:{date}
 
-    return getApi('/api/mock/services/summary',params);
+    return getApi('/api/v1/services/summary',params);
 }
 
 //现金流
 export function loadCashFlow(companyid ,date='') {
     let params = companyid?{companyid,date}:{date}
 
-    return getApi('/api/mock/services/cashflow',params);
+    return getApi('/api/v1/services/cashflow',params);
 }
 //利润表
 export function loadProfit(companyid,date='') {
     let params = companyid?{companyid,date}:{date}
 
-    return getApi('/api/mock/services/profit',params);
+    return getApi('/api/v1/services/profit',params);
 }
 
 //应收账款 应付账款
 export function loadAccounts(companyid,date='',type='1') {
     let params = companyid?{companyid,date,type}:{date,type}
 
-    return getApi('/api/mock/services/accounts',params);
+    return getApi('/api/v1/services/accounts',params);
 }
 
 //纳税表
