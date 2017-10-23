973c9c1c3d31068f1c20c036366082c1579f1de9
jinglan1212@icloud.com <zhangjl@i-counting.cn>
2017-10-23 15:21:09 +0800

    咨询留言联调完成

--- app/apis/feedBack.js
+++ app/apis/feedBack.js
@@ -10,5 +10,10 @@ import {postApi} from './common';
 
 export function submitFeedBack(from = '',city = '',name = '',tel = '',text = '') {
     //type 1.产品页 2 APP反馈 3 核名电话   producname 产品名称  text 留言内容
-    return postApi('/api/mock/feedbacks',{from,city,name,tel,text});
+    return postApi('/api/v1/feedbacks',{from,city,name,tel,text});
 }
+
+
+
+
+
