1d85f9b0a3dc769b50dc9fe0490e78f06b49a941
jinglan1212@icloud.com <zhangjl@i-counting.cn>
2017-12-06 11:28:18 +0800

    友盟统计IOS版本号修改

--- ios/corpapp/AppDelegate.m
+++ ios/corpapp/AppDelegate.m
@@ -36,7 +36,8 @@ static BOOL isProduction = true;  //填写isProdurion  平时测试时为false �
   
    /** 友盟统计 */
   UMConfigInstance.appKey = @"59f6dbeea40fa3718e000108";
-  
+  NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+  [MobClick setAppVersion:version];
   //  UMConfigInstance.eSType=E_UM_GAME;//友盟游戏统计，如不设置默认为应用统计
   
   [MobClick startWithConfigure:UMConfigInstance];
