0910813e4a1fdeb55bf9cf1e317ba05c2ce87df7
zhuangzihao <zhuangzh@i-counting.cn>
2018-01-04 17:53:57 +0800

    [feat] 修改服务业标题 related to issue #47

--- app/service/service/ServicePage.js
+++ app/service/service/ServicePage.js
@@ -299,7 +299,7 @@ export default class ServicePage extends BComponent {
                             tapBackgroundToDismiss:true
                         }
                     })}>
-                        <View style={{width:deviceWidth*0.7,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                        <View style={{width:deviceWidth*0.55,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
                             <Text numberOfLines={1} style={{fontSize:setSpText(18),fontWeight:'bold',textAlign:'center',color:'white'}}>{this.state.title.length>10?this.state.title.substr(0,10)+'...':this.state.title}&#12288;</Text>
                             <Image source={require('../../img/change_arrow_white.png')}/>
                         </View>
@@ -331,7 +331,7 @@ export default class ServicePage extends BComponent {
                             },
                         }]);
                 }}>
-                    <View style={{width:deviceWidth*0.5,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                    <View style={{width:deviceWidth*0.55,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
                         <Text numberOfLines={1} style={{fontSize:setSpText(18),fontWeight:'bold',textAlign:'center',color:'white'}}>{this.state.title.length>10?this.state.title.substr(0,10)+'...':this.state.title}&#12288;</Text>
                         <Image   source={require('../../img/change_arrow_white.png')}/>
                     </View>
