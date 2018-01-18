578e741a3645865c3f73f93c5077142302b6f447
BeanSoft <beansoft@126.com>
2018-01-18 15:42:50 +0800

    [feat] 增加判断字符串非空的函数更新注释 related to issue #77

--- app/util/StringUtil.js
+++ app/util/StringUtil.js
@@ -1,5 +1,9 @@
 /**
  * 判断字符串不为空的函数
+ * 原因:
+ * let img = "";
+ console.log( img && (img.length > 0) );
+ 返回结果是 "" (空白字符串), 等价于 true, 会导致后续代码执行.
  * @param str
  */
 export default function notEmpty(str) {
