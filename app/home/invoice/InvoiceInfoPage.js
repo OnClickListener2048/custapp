9bca179162bcf0e9c896ea1ba3d7813f3db5ce25
zhuangzihao <zhuangzh@i-counting.cn>
2018-01-23 11:55:31 +0800

    [feat]添加发票验真api related in issue #47

--- app/home/invoice/InvoiceInfoPage.js
+++ app/home/invoice/InvoiceInfoPage.js
@@ -15,6 +15,7 @@ import {
 import {SCREEN_HEIGHT,SCREEN_WIDTH} from '../../config';
 import BComponent from '../../base'
 import invoice_data from './invoice_data.json'
+import * as apis from '../../apis';
 export default class InvoiceInfoPage extends BComponent {
 
     constructor(props){
@@ -27,8 +28,38 @@ export default class InvoiceInfoPage extends BComponent {
     };
     //开始扫描
     componentDidMount() {
+        // let params = {
+        //     FPDM:this.props.codeInputValue,//arr[2]发票代码
+        //     FPHM:this.props.numberInputValue,//arr[3]发票号码
+        //     KPRQ:this.props.dateTime,//arr[5]日期
+        //     FPLX:this.props.invoiceType,//arr[1]发票类型
+        // }
+        // if(this.props.amount){
+        //     params.FPJE = this.props.amount
+        // }
+        // if(this.props.checkCodeInputValue){
+        //     params.JYM = this.props.checkCodeInputValue
+        // }
 
+        //const res = {result:"01,10,011001600211,25236205,33.96,20170701,70438474151372413936,AD3C,"};
+        let params = {
+            FPDM:'011001600211',//arr[2]发票代码
+            FPHM:'25236205',//arr[3]发票号码
+            KPRQ:'20170701',//arr[5]日期
+            FPLX:'10',//arr[1]发票类型
+            FPJE:'33.96',//arr[4]发票金额
+            JYM:'413936'//arr[6]校验码后六位
+        }
+        apis.verifyInvoice('2',params).then((responseData)=>{
+
+            console.log('success:',responseData)
+
+        },(e)=>{
+            console.log('error',e)
+
+        })
     }
+
 
     render(){
         return(
