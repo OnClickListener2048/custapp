69af470bd6d3bcd741de1b3493d4780e117acc89
liufei <liufei@icounting.cn>
2018-03-26 17:59:51 +0800

    [feat]获取企业，订单，抬头的个数，接口单元测试修改 related in issue #138

--- __tests__/apis/mine.test.js
+++ __tests__/apis/mine.test.js
@@ -9,10 +9,12 @@ import TestHttpAdapter from "../../__mocks__/TestHttpAdapter";
 
 
 it('获取企业，订单，抬头的个数', async () => {
-    TestHttpAdapter.enableLogin = true;
+    TestHttpAdapter.enableLogin = true;//请求需要token
     try {
     let result =  await apis.loadMinePageNumber();
     console.log("*************", result);
+    //数据正常返回内容为{ code: 0,data: { companyCount: 0, orderCount: 0, titleCount: 0 } }
+
     expect(result.code).toEqual(0);
     // expect(result.msg).toEqual("登录失败，请稍后重试");
     } catch (e) {
