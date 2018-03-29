eb4c5e09f681e05036af8ec4348071f2fcdc220d
BeanSoft <beansoft@126.com>
2018-03-29 14:17:46 +0800

    [refactor] 增加通用安卓包, 支持x86 安卓架构 relative issue #120

--- build_android_channel.sh
+++ build_android_channel.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -jar android/walle-cli-all.jar batch -c 360,qq,xiaomi,huawei,oppo,vivo android/app/build/outputs/apk/li-armeabi-v7a-release.apk
+# 打安卓渠道包
+java -jar android/walle-cli-all.jar batch -c 360,qq,xiaomi,huawei,oppo,vivo android/app/build/outputs/apk/li-release.apk
